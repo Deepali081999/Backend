c920648dd1c5fddb033a420c23ff719c42c6df68
Bhoyar <919820@cognizant.com>
2022-09-17 16:04:30 +0530

    cors policy

--- src/main/java/com/tweetapp/likes/LikesController.java
+++ src/main/java/com/tweetapp/likes/LikesController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin
 @RestController
 public class LikesController {
 	
